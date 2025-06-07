bb4c352bf2f68be30dbcde09b3ae003d7ddbbcf4
skavorodka <flipak180@mail.ru>
2025-06-07 23:24:52 +0300

    +++

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -2,23 +2,32 @@
 import {onMounted, Ref, ref, useTemplateRef} from "vue";
 import {IonIcon} from '@ionic/vue';
 import {send} from "ionicons/icons";
+import {Haptics, NotificationType} from "@capacitor/haptics";
+import helpers from "@/helpers";
 
 const autofocus: Ref = useTemplateRef('autofocus');
 const answer = ref('')
+const error = ref(false)
 
 onMounted(() => {
     setTimeout(() => {
-        autofocus.value.focus()
+        setFocus()
     }, 500)
 })
 
-function submitAnswer() {
-    console.log(answer.value);
+async function submitAnswer() {
+    error.value = false;
+    if (Math.floor((Math.random() * 100) + 1) % 2) {
+        //
+    } else {
+        error.value = true;
+        await Haptics.notification({ type: NotificationType.Error });
+    }
     answer.value = '';
-    autofocus.value.focus()
+    setFocus()
 }
 
-function handleBlur() {
+function setFocus() {
     autofocus.value.focus()
 }
 </script>
@@ -26,22 +35,49 @@ function handleBlur() {
 <template>
     <div class="question">
         <div class="question__grid ion-padding ion-margin-bottom">
-            <div class="answer_item a">A</div>
-            <div class="answer_item b">B</div>
-            <div class="answer_item c">C</div>
-            <div class="answer_item d">D</div>
-            <div class="answer_item e">E</div>
-            <div class="answer_item f">F</div>
-            <div class="answer_item g">G</div>
-            <div class="answer_item h">H</div>
-            <div class="answer_item f">I</div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">10%</div>
+                <div class="answer__text" v-show="false">Печенье</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">50%</div>
+                <div class="answer__text" v-show="false">Мороженое</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">3%</div>
+                <div class="answer__text" v-show="false">Кукурузные палочки</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">12%</div>
+                <div class="answer__text" v-show="false">Буше</div>
+            </div>
+            <div class="answer opened" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">3%</div>
+                <div class="answer__text" v-show="true">Шоколадный батончик</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">1%</div>
+                <div class="answer__text" v-show="false">Мармелад</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">21%</div>
+                <div class="answer__text" v-show="false">Пирожное</div>
+            </div>
+            <div class="answer opened" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">16%</div>
+                <div class="answer__text" v-show="true">Торт</div>
+            </div>
+            <div class="answer" :style="{backgroundColor: helpers.getRandomColor()}">
+                <div class="answer__percent">32%</div>
+                <div class="answer__text" v-show="false">Зефир</div>
+            </div>
         </div>
-        <div class="answer">
-            <input v-model="answer" type="text" inputmode="text" aria-label="Ответ" ref="autofocus" @blur="handleBlur">
+        <form :class="{shake: error}" @submit.prevent="submitAnswer">
+            <input v-model="answer" type="text" inputmode="text" aria-label="Ответ" ref="autofocus" @blur="setFocus">
             <div class="button" @click="submitAnswer">
                 <ion-icon slot="icon-only" :icon="send"></ion-icon>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -51,21 +87,19 @@ function handleBlur() {
     &__grid {
         display: grid;
         grid-template-columns: repeat(4, 150px);
-        grid-gap: 10px;
+        grid-gap: 4px;
         grid-auto-flow: row dense;
         text-align: center;
         overflow-x: scroll;
     }
 }
 
-.answer {
+form {
     position: fixed;
     bottom: var(--ion-safe-area-bottom, 0);
     left: 0;
     width: 100%;
-    background: rgb(13, 13, 13);
-    border-top: 0.8px solid rgb(38, 38, 38);
-    padding: 5px;
+    padding: 10px;
 
     input {
         background: var(--grey);
@@ -95,44 +129,69 @@ function handleBlur() {
     }
 }
 
-.answer_item {
+.answer {
     padding: 40px;
-    font-size: 1.5em;
     border: 1px solid black;
-}
-
-.b {
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
     background: violet;
-    padding: 100px 0;
-    grid-area: 2 / 1 / 4 / 3;
+    width: 150px;
+    height: 150px;
+    font-size: 24px;
+
+    &__percent {
+        border: 5px solid #fff;
+        padding: 5px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        border-radius: 50%;
+        width: 75px;
+        height: 75px;
+        min-width: 75px;
+        min-height: 75px;
+    }
+
+    &__text {
+        font-size: 18px;
+    }
+
+    &.opened {
+        font-size: 18px;
+    }
+
+    &.opened &__percent {
+        border: 3px solid #fff;
+        padding: 3px;
+        width: 50px;
+        height: 50px;
+        min-width: 50px;
+        min-height: 50px;
+    }
 }
 
-.a {
-    background: violet;
-    grid-area: 1 / 2 / 2 / 3;
+.shake {
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+    transform: translate3d(0, 0, 0);
 }
 
-.c {
-    background: orange;
-    grid-row-start: 1;
-    grid-row-end: 3;
-}
-
-.d {
-    background: orange;
-    grid-row-start: 1;
-    grid-row-end: 2;
-}
-
-.g {
-    background: lightgreen;
-    grid-column-start: 3;
-    grid-column-end: 5;
-}
-
-.h {
-    background: lightgreen;
-    grid-column-start: 2;
-    grid-column-end: 4;
+@keyframes shake {
+    10%, 90% {
+        transform: translate3d(-1px, 0, 0);
+    }
+    20%, 80% {
+        transform: translate3d(2px, 0, 0);
+    }
+    30%, 50%, 70% {
+        transform: translate3d(-4px, 0, 0);
+    }
+    40%, 60% {
+        transform: translate3d(4px, 0, 0);
+    }
 }
 </style>
